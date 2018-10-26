1a3ecc4fe2303fde3212bf02fda1dbe28d0d317a
Drashna Jaelre <drashna@live.com>
2018-10-25 22:08:49 -0700

    Add Bootmagic Lite for now

--- users/drashna/config.h
+++ users/drashna/config.h
@@ -72,3 +72,12 @@
 #define DISABLE_LEADER
 
 #define MACRO_TIMER 5
+
+#if !defined(BOOTMAGIC_LITE) && !defined(BOOTMAGIC_ENABLE)
+#ifndef BOOTMAGIC_LITE_COLUMN
+  #define BOOTMAGIC_LITE_COLUMN 0
+#endif
+#ifndef BOOTMAGIC_LITE_ROW
+  #define BOOTMAGIC_LITE_ROW 0
+#endif
+#endif
