add736dc80c6a43eeb56281645f3799c05647953
Jason Yamada-Hanff <jyamada1@gmail.com>
2019-04-10 00:39:31 -0700

    (dz60:jyh) fix: correct spelling of hyper key

--- keyboards/dz60/keymaps/jyh/keymap.c
+++ keyboards/dz60/keymaps/jyh/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *
    * with Hold (Emacs-oriented)
    * ,-------------------------------------------------------------.
-   * | Gui |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+   * | Gui |   |   |   |   |   |   |   |   |   |   |   |   |Hyp|Del|
    * |-------------------------------------------------------------|
    * |       |   |   |   |   |   |   |   |   |   |   |   |   | Ctrl|
    * |-------------------------------------------------------------|
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	     KC_LCTL   ,   KC_LGUI   ,  KC_LALT   ,     CTL_BSP     ,     PRI    ,       KC_SPC       ,  KC_RALT,   SEC , KC_LEFT, KC_DOWN, KC_RGHT),
 
 	[_PRI] = LAYOUT_directional(
-		KC_ESC,   KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12 , KC_DEL , _______,
+		KC_ESC,   KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12 , KC_HYPR , KC_DEL,
     	  _______  ,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5 , _______, KC_6 ,  KC_7  ,  KC_8  ,  KC_9  , KC_SLSH, KC_ASTR,   _______   ,
      	    _______   , KC_LEFT,  KC_UP , KC_DOWN, KC_RGHT, _______, _______, _______,  KC_4  ,  KC_5  ,  KC_6  , KC_MINS,       _______     ,
     	         _______, _______, _______, _______, _______, _______, _______, KC_COMM,  KC_1  ,  KC_2  ,  KC_3  , KC_PLUS, _______, _______,
