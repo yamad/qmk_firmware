5f674c7df653d36db193766cc1749d7c2d556232
Jason Yamada-Hanff <jyamada1@gmail.com>
2019-04-10 00:34:55 -0700

    jyh dz60 layout: convert to 2U left shift
    the 1U shifts are very hard to get used to. turns out I press the shift keys
    on a standard qwerty about right in the middle

--- keyboards/dz60/keymaps/jyh/keymap.c
+++ keyboards/dz60/keymaps/jyh/keymap.c
@@ -46,15 +46,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Base Layer (Default Layer)
    *
    * ,----------------------------------------------------------------.
-   * | ` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |   Bkspce   |
+   * | ` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = | Meh | Bksp |
    * |----------------------------------------------------------------|
    * |  Tab   | q | w | e | r | t | y | u | i | o | p | [ | ] |   /   |
    * |----------------------------------------------------------------|
-   * |   Ctrl  | a | s | d | f | g | h | j | k | l | ; | ' |  enter   |
+   * |  Ctrl   | a | s | d | f | g | h | j | k | l | ; | ' |  enter   |
    * |----------------------------------------------------------------|
-   * | Hypr| LS | z | x | c | v | b | n | m | , | . | / | RS | ^ | FN |
+   * |  Shift   | z | x | c | v | b | n | m | , | . | / | RS | ^ |PRI |
    * |----------------------------------------------------------------|
-   * | Meh | Gui | Alt |  Back  | FN |  Space  | Alt | FN | < | v | > |
+   * | Ctl | Gui | Alt |  Back | PRI |  Space  | Alt | FN | < | v | > |
    * `----------------------------------------------------------------'
    *
    * with Hold (Emacs-oriented)
@@ -65,30 +65,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------------------------------------------------------------|
    * |  Ctrl  |   |   |   |   |   |   |   |   |   |   |   | Enter  |
    * |-------------------------------------------------------------|
-   * | Hypr|   |   |   |   |   |   |   |   |   |   |   |   | ^ |FN1|
+   * |         |   |   |   |   |   |   |   |   |   |   |   | ^ |PRI|
    * |-------------------------------------------------------------|
-   * | Meh | Gui | Alt | Ctrl | FN1|        | Alt | FN2| < | v | > |
+   * |     | Gui | Alt | Ctrl | PRI |       | Alt | FN2| < | v | > |
    * `-------------------------------------------------------------'
    */
-	[_QW] = LAYOUT_all(
-	    GUI_GRV ,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,  KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  , KC_MINS, KC_EQL ,  KC_NO , KC_BSPC,
+	[_QW] = LAYOUT_directional(
+	    GUI_GRV ,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,  KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  , KC_MINS, KC_EQL ,  KC_MEH , KC_BSPC,
 	      KC_TAB   ,  KC_Q  ,  KC_W  ,  KC_E  ,  KC_R  ,  KC_T  ,  KC_Y  ,  KC_U  ,  KC_I   ,  KC_O  ,  KC_P  , KC_LBRC, KC_RBRC,   KC_BSLS   ,
 		    CTL_ESC   ,  KC_A  ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G  ,  KC_H  ,  KC_J  ,  KC_K  ,  KC_L  , KC_SCLN, KC_QUOT,       KC_ENT       ,
-	    KC_HYPR , KC_LSFT,  KC_Z  ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  ,  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,  KC_UP  ,  PRI  ,
-	     KC_MEH   ,   KC_LGUI   ,  KC_LALT   ,     CTL_BSP     ,     PRI    ,       KC_SPC       ,  KC_RALT,   SEC , KC_LEFT, KC_DOWN, KC_RGHT),
+	          KC_LSFT,      KC_Z  ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  ,  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,  KC_UP  , PRI   ,
+	     KC_LCTL   ,   KC_LGUI   ,  KC_LALT   ,     CTL_BSP     ,     PRI    ,       KC_SPC       ,  KC_RALT,   SEC , KC_LEFT, KC_DOWN, KC_RGHT),
 
-	[_PRI] = LAYOUT_all(
+	[_PRI] = LAYOUT_directional(
 		KC_ESC,   KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12 , KC_DEL , _______,
-    	  _______  , _______, _______, _______, _______, _______, _______, _______,  KC_7  ,  KC_8  ,  KC_9  , KC_SLSH, KC_ASTR,   _______   ,
+    	  _______  ,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5 , _______, KC_6 ,  KC_7  ,  KC_8  ,  KC_9  , KC_SLSH, KC_ASTR,   _______   ,
      	    _______   , KC_LEFT,  KC_UP , KC_DOWN, KC_RGHT, _______, _______, _______,  KC_4  ,  KC_5  ,  KC_6  , KC_MINS,       _______     ,
-    	_______, _______, _______, _______, _______, _______, _______, _______, KC_COMM,  KC_1  ,  KC_2  ,  KC_3  , KC_PLUS, _______, _______,
+    	         _______, _______, _______, _______, _______, _______, _______, KC_COMM,  KC_1  ,  KC_2  ,  KC_3  , KC_PLUS, _______, _______,
     	  _______  ,  _______   ,  _______   ,     _______     ,  _______  ,       _______      ,  KC_0  , KC_DOT , KC_ENT , _______, _______),
 
-	[_SEC] = LAYOUT_all(
+	[_SEC] = LAYOUT_directional(
     	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
    		  _______  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, MKITPNK, _______, _______, _______,    RESET    ,
      	    _______   , _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______,       _______     ,
-	    _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, KC_INS , KC_HOME, KC_PGUP,
+	          _______   , _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, KC_INS , KC_HOME, KC_PGUP  ,
     	  _______  ,  _______   ,  _______   ,     _______     ,   KC_NO   ,       _______      , _______, _______, KC_DEL , KC_END , KC_PGDN)
 };
 
