2c504711d84d02fb11225f2c24597fed81c718a3
Jason Yamada-Hanff <jyamada1@gmail.com>
2019-05-19 20:05:39 -0700

    remove control key tap-hold on quote, which often goes wrong

--- keyboards/ergodash/rev1/keymaps/jyh/keymap.c
+++ keyboards/ergodash/rev1/keymaps/jyh/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
 	GUI_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                        KC_EQL , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    GUI_BSL, \
     MEH_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-	CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,                         KC_ENT , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_QUO, \
+	CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,                         KC_ENT , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     SFT_MIN, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL,                        KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_EQL, \
 	CTL_TAB, KC_LGUI, KC_LALT, LOWER,            CTL_BSP, KC_LSFT,KC_LGUI,        KC_RSFT,KC_LALT, KC_SPC,           RAISE,   KC_DOWN, KC_UP,   HPR_ENT  \
   ),
