18133c42a730308fee2f4c0a641f8ab37598624c
Jason Yamada-Hanff <jyamada1@gmail.com>
2019-08-18 14:48:29 -0700

    (ergodash/mini:jyh) make symbol layer and thumb cluster more useful

--- keyboards/ergodash/mini/keymaps/jyh/keymap.c
+++ keyboards/ergodash/mini/keymaps/jyh/keymap.c
@@ -23,7 +23,8 @@ enum custom_keycodes {
 #define SFT_MIN MT(MOD_LSFT, KC_MINS) //  -       Left Shift
 #define SFT_PLS MT(MOD_LSFT, KC_PLUS) //  +       Left Shift
 #define CTL_TAB MT(MOD_LCTL, KC_TAB)  //  Tab     Left Control
-#define GUI_LBC MT(MOD_LGUI, KC_LBRC) //  [       Left Meta
+#define GUI_LBC MT(MOD_LGUI, KC_LBRC) //  [       Left Gui
+#define ALT_RBC MT(MOD_LALT, KC_RBRC) //  ]       Left Alt
 
 // Middle / Thumbs
 #define CTL_BSP MT(MOD_LCTL, KC_BSPC)  // Bkspce  Left Control
@@ -37,43 +38,56 @@ enum custom_keycodes {
 #define GUI_RBC MT(MOD_RGUI, KC_RBRC) //  ]       Right Meta
 #define HPR_ENT MT(MOD_HYPR, KC_ENT)  //  Enter   Hyper
 
+enum {
+	TD_RGHT_ENT = 0
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |  Esc |   Q  |   W  |   E  |   R  |   T  |   -  |                    |   =  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * |  `   |   Q  |   W  |   E  |   R  |   T  |  [   |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   A  |   S  |   D  |   F  |   G  |   [  |                    |   ]  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | Esc  |   A  |   S  |   D  |   F  |   G  | Tab  |                    | Enter|   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  | Space|                    | Enter|   N  |   M  |   ,  |   .  |   /  | Shift|
+   * |  -   |   Z  |   X  |   C  |   V  |   B  | Shift|                    | Shift|   N  |   M  |   ,  |   .  |   /  |  =   |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Lower| Space|Delete|||||||| Bksp | Enter| Raise|||||||| Left | Down |  Up  | Right|
+   * | Tab  |   [  |   ]  | Super|||||||| Lower| Bksp | Ctrl |||||||| Alt  | Space| Raise|||||||| Left | Down |  Up  | Right|
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   *
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * | Gui  |   Q  |   W  |   E  |   R  |   T  |  [   |                    |   ]  |   Y  |   U  |   I  |   O  |   P  | Gui/Shift|
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  | Tab  |                    | Enter|   H  |   J  |   K  |   L  |   ;  |  "   |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | Shift|                    | Shift|   N  |   M  |   ,  |   .  |   /  | Shift|
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * | Ctrl |  GUI | Meta | Super|||||||| Lower| Ctrl | Ctrl |||||||| Alt  | Space| Raise|||||||| Left | Down |  Up  | Enter|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
     GUI_GRV, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    GUSH_BS, \
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,                         KC_ENT,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     SFT_MIN, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT,                        KC_RSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_EQL, \
-    CTL_TAB, GUI_LBC, KC_LALT, KC_MEH,           LOWER,   CTL_BSP, KC_LALT,        KC_ENT,KC_SPC, RAISE,             KC_HYPR, KC_RALT, GUI_RBC, KC_ENT   \
+    CTL_TAB, GUI_LBC, ALT_RBC, KC_MEH,           LOWER,   CTL_BSP, KC_LCTL,       KC_RALT,KC_SPC, RAISE,             KC_LEFT, KC_DOWN, KC_UP, TD(TD_RGHT_ENT) \
   ),
 
   /* Lower
    * ,------------------------------------------------.                    ,------+-----------------------------------------.
    * |      |  !   |  @   |  #   |  $   |  %   |      |                    |      |  ^   |  &   |  *   |  (   |  )   |      |
    * |------+------+------+------+------+------+------|                    |------+------+------+------+------+------+------|
-   * |      |  <M  | MPlay|  M>  |      |      |      |                    |      | Left | Down |  Up  | Right|      |      |
+   * |      |  -   |  '   |  `   |  =   |      |      |                    |      | Left | Down |  Up  | Right|      |      |
    * |------+------+------+------+------+------+------|                    |------+------+------+------+------+------+------|
-   * |      | Vol- | Mute | Vol+ |      |      |      |                    |      | Home | PgDn | PgUp | End  |      |      |
+   * |      |  _   |  "   |  ~   |  +   |      |      |                    |      | Home | PgDn | PgUp | End  |      |      |
    * |------+------+------+------+------+------+------+------.      ,------|------+------+------+------+------+------+------|
-   * |MS_BT2|MS_BT3|MS_BT1|      ||||||||Adjust|      |      |      |      |      | RAISE||||||||      |      |      |      |
+   * |MS_BT2|MS_BT3|MS_BT1|      ||||||||LOWER |      |      |      |      |      |Adjust||||||||      |      |      |      |
    * `-------------------------------------------------------.      `-------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                        _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-    _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_HOME, KC_PGUP, _______,                        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-    _______, KC_VOLD, KC_MUTE, KC_VOLD, KC_END,  KC_PGDN, _______,                        _______, _______, _______, _______, _______, _______, _______, \
-    KC_MS_BTN2, KC_MS_BTN3, KC_MS_BTN1, _______,   LOWER, _______, _______,       _______,_______, _______,          _______, _______, _______, _______ \
+    _______, KC_MINS, KC_QUOT, KC_GRV,  KC_EQL,  KC_PGUP, _______,                        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+    _______, KC_UNDS, KC_DQUO, KC_TILD, KC_PLUS, KC_PGDN, _______,                        _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, \
+    KC_MS_BTN2, KC_MS_BTN3, KC_MS_BTN1, _______,   LOWER, _______, _______,       _______,_______, _______,          _______, _______, _______, _______  \
   ),
 
   /* Raise
@@ -94,13 +108,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______, _______, _______, _______,          _______, _______, _______,       _______,_______, _______,          KC_DOT , KC_0   , KC_PLUS, _______
     ),
 
-  /* Adjust */
+  /* Adjust
+  * ,------------------------------------------------.                    ,------+-----------------------------------------.
+  * |      | Reset|      |      |      |      |      |                    |      |      |  F7  |  F8  |  F9  | F10  | Del  |
+  * |------+------+------+------+------+------+------|                    |------+------+------+------+------+------+------|
+  * |      |      | < M  | Play |  M > | Home |      |                    |      | PgUp |  F4  |  F5  |  F6  | F11  |      |
+  * |------+------+------+------+------+------+------|                    |------+------+------+------+------+------+------|
+  * |      |      | Vol- | Mute | Vol+ | End  |      |                    |      | PgDn |  F1  |  F2  |  F3  | F12  |      |
+  * |------+------+------+------+------+------+------+------.      ,------|------+------+------+------+------+------+------|
+  * |      |      |      |      ||||||||ADJUST|      |      |      |      |      |ADJUST||||||||      |      |      |      |
+  * `-------------------------------------------------------.      `-------------------------------------------------------.
+  */
   [_ADJUST] = LAYOUT(
-    _______, RESET  , _______, _______, _______, _______,_______,                       _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______, \
-    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F12,  _______, \
+    _______, RESET  , _______, _______, _______, _______,_______,                       _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL , \
+    _______, _______, KC_MPRV, KC_MPLY, KC_MNXT,KC_HOME, _______,                       _______, KC_PGUP, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______, \
+    _______, _______, KC_VOLD, KC_MUTE, KC_VOLU,KC_END,  _______,                       _______, KC_PGDN, KC_F1,   KC_F2,   KC_F3,   KC_F12,  _______, \
     _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______  \
   )
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+	[TD_RGHT_ENT] = ACTION_TAP_DANCE_DOUBLE(KC_ENT, KC_RGHT)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
