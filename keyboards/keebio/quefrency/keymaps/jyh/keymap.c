67d6cf45195ef6b17b5fa31890e099f63fb5b90f
Jason Yamada-Hanff <jyamada-hanff@chromologic.com>
2021-03-23 10:00:22 -0700

    move right bottom row key assignments to the correct location

--- keyboards/keebio/quefrency/keymaps/jyh/keymap.c
+++ keyboards/keebio/quefrency/keymaps/jyh/keymap.c
@@ -9,9 +9,10 @@ extern keymap_config_t keymap_config;
 #define _BASE 0
 #define _FN1 1
 
-#define GUI_GRV MT(MOD_LGUI, KC_GRV)  //  `       Meta
+#define GUI_GRV MT(MOD_LGUI, KC_GRV)  //  `
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)  //  Esc     Left Control
-#define CTL_BSP MT(MOD_LCTL, KC_BSPC)  // Bkspce  Left Control
+#define CTL_BSP MT(MOD_LCTL, KC_BSPC) //  Bkspce  Left Control
+#define ALT_TAB MT(MOD_LALT, KC_TAB)  //  Tab     Left Alt
 
 
 enum custom_keycodes {
@@ -24,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_MPRV, KC_MNXT, KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
 		KC_F1  , KC_F2  , CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
 		KC_F3  , KC_F4  , KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-		KC_F5  , KC_F6  , KC_MEH , KC_LGUI, KC_LALT, CTL_BSP, MO(_FN1),KC_SPC,  KC_SPC, _______, KC_RCTL, MO(_FN1), KC_LEFT, KC_DOWN, KC_RGHT
+		KC_F5  , KC_F6  , KC_MEH , KC_LGUI, KC_LALT, CTL_BSP, ALT_TAB, KC_SPC,  KC_SPC, KC_RCTL, MO(_FN1), _______, KC_LEFT, KC_DOWN, KC_RGHT
 		),
 
 	[_FN1] = LAYOUT_65_with_macro(
